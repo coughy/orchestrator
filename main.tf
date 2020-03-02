b7fa59d9794184ab479988b08cd505528c1c3819
coughy <coughy_2@hotmail.com>
2020-03-02 13:07:03 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+}
+# Create a resource group
+
+resource "azurerm_resource_group" "example" {
+  name     = "testResourceGroup1"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
+}
